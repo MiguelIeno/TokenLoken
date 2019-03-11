004107ec66800cd7d7951b8a06d4d19c4567a443
mike <maieno@hotmail.com>
2019-03-11 19:55:41 -0300

    continuamos con el token

--- contracts/TokenLoken.sol
+++ contracts/TokenLoken.sol
@@ -1,19 +1,39 @@
-pragma solidity ^0.4.24;
+pragma solidity ^0.4.0;
 import  "../node_modules/openzeppelin-solidity/contracts/token/ERC20/ERC20.sol";
 import "../node_modules/openzeppelin-solidity/contracts/token/ERC20/ERC20Detailed.sol";
+import "../node_modules/openzeppelin-solidity/contracts/token/ERC20/Owned.sol";
 
 
 /**
 *  Token Loken
 * Autor : Miguel Ieno
 */
-contract TokenLoken is ERC20 , ERC20Detailed{
+contract TokenLoken is ERC20 , ERC20Detailed , Owned{
 
-	constructor (string name , string symbol, uint decimal)
-			ERC20Detailed(name , symbol, decimal) public {
-
-			}
 
     
-    
+    constructor(
+        string name,
+        string  symbol,
+        uint8  decimales,
+        uint256 tokensInit
+    ) 
+        public 
+        ERC20Detailed(name, symbol, decimales)
+        ERC20()
+        Owned()
+    {
+		tokensInit = INITIAL_SUPPLY * (10 ** uint (decimales));
+		balances[msg.sender] = tokensInit;
+        _mint(owner, tokensInit); // assigning all tokens to owner initially.
+    }
+
+	function setON(string _n) public onlyOwner returns (bool) {
+    Owner = _n;
+    return true;
+  }
+
+    function getON() public view returns (string) {
+    return Owner;
+  }
 }
